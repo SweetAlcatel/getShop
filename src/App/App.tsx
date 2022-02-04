c6ab055ecf5bc80820c33abe034f2859790aa2cf
sweetalcatel <mrneey@gmail.com>
2022-02-04 12:34:16 +0300

    реализована логика открытия окна

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,17 +2,19 @@ import React, { useState } from 'react';
 import './App.css';
 import { Video } from '../Video/Video';
 import { Banner } from '../Banner/Banner';
+import { TelephoneNumberWindow } from '../TelephoneNumberWindow/TelephoneNumberWindow';
 
 export const App = () => {
 
     const [isShow, setShow] = useState(false);
 
-    console.log(isShow);
+    const [isClicked, setClick] = useState(false);
 
     return (
         <main>
             <Video setState={setShow}/>
-            {isShow && <Banner />}
+            {isShow && <Banner setClick={setClick}/>}
+            {isClicked && <TelephoneNumberWindow />}
         </main>
     );
 
--- src/Banner/Banner.tsx
+++ src/Banner/Banner.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../Banner/Banner.css';
 
-export const Banner: React.FC = () => {
+interface BannerProps {
+    setClick: (prop: boolean) => void
+}
 
-    const getTelephoneNumber = () => {
-        
-    }
+export const Banner = ({ setClick } : BannerProps) => {
 
     return (
         <div className='banner'>
             <h1>Исполните мечту вашего Ван Дамма! Подарите ему два грузовика VOLVO</h1>
             <img alt='Тут куар-код'/>
             <span>Сканируйте QR-код или нажмите кнопку ОК</span>
-            <button onClick={() => getTelephoneNumber()}>ОК</button>
+            <button onClick={() => setClick(true)}>ОК</button>
         </div>
     );
 };
